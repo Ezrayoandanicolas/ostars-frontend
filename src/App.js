a6d9416fc27a6723ecf8fc43c4e07d9fef2b0ffc
Ezra Yoanda Nicolas <ezrayoandanicolas@gmail.com>
2023-09-11 13:28:44 +0700

    Add Router & Meta Pixel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
+import {Routes, Route} from 'react-router-dom'
 // import Logo from './logo.svg';
 import React from 'react'
 import { HelmetProvider  } from 'react-helmet-async';
 import MetaTag from './components/MetaTag'
 import Navbar from './components/NavbarDefault'
-import Header from './components/Header'
-// import AboutMe from './components/AboutMe'
-import Product from './components/Product'
-import Services from './components/Services'
-import Keunggulan from './components/Keunggulan'
-import TermsAndCondition from './components/TermsAndCondition'
+import MasterLanding from './components/MasterLanding'
 // import ReactDOM from 'react-dom'
 import './App.css';
 
@@ -18,14 +14,11 @@ function App() {
       <MetaTag />
       <div className="mx-auto bg-white round-xl shadow border m-0 p-0 overflow-hidden scroll-smooth">
         <Navbar />
-        <Header />
-        {/* <AboutMe /> */}
-        <Product />
-        <Services />
-        <Keunggulan />
-        <TermsAndCondition />
+        <Routes>
+          <Route path='/' element={<MasterLanding />}></Route>
+        </Routes>
       </div>
-    </HelmetProvider>
+      </HelmetProvider>
   );
 }
 
---
+++ src/components/MasterLanding.jsx
@@ -0,0 +1,21 @@
+import Header from "./Header";
+// import AboutMe from "./AboutMe";
+import Product from "./Product";
+import Services from "./Services";
+import Keunggulan from "./Keunggulan";
+import TermsAndCondition from "./TermsAndCondition";
+
+const Landing = () => {
+    return (
+        <>
+            <Header />
+            {/* <AboutMe /> */}
+            <Product />
+            <Services />
+            <Keunggulan />
+            <TermsAndCondition />
+        </>
+    )
+}
+
+export default Landing
